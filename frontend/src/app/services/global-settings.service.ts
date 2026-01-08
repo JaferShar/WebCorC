9e14d3b865e917593875a76f72e547b0d162dd42
Jafer Sharfeddine <uowjj@student.kit.edu>
2026-01-08 17:16:46 +0100

    removed unwanted features and added reset variant to global settings

--- frontend/src/app/services/global-settings.service.ts
+++ frontend/src/app/services/global-settings.service.ts
@@ -1,10 +1,20 @@
-import { Injectable } from "@angular/core";
+import {Injectable, signal} from "@angular/core";
+import {ResetVariant} from "../types/ResetVariant";
 
 @Injectable({
-  providedIn: "root",
+    providedIn: "root",
 })
 export class GlobalSettingsService {
   public conditionsAlwaysOpen: boolean = true;
   public showMiniMap: boolean = false;
   public isVerifying: boolean = false;
+    private _resetVariant = signal<ResetVariant>(
+        ResetVariant.ReingoldTilford
+    );
+
+    readonly resetVariant = this._resetVariant.asReadonly();
+
+    setResetVariant(variant: ResetVariant) {
+        this._resetVariant.set(variant);
+    }
 }
